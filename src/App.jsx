83b697d6f2dc63952070f119d23375d2e5d4dd2e
Gautierpicon <gautierpicon@proton.me>
2025-03-05 11:43:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Flag from "./components/Flag";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Flag />
+    </div>
+  )
+} 
+
+export default App
