81145969ce9612fd4946b024cd181b889c89afc8
Gautierpicon <gautierpicon@proton.me>
2025-11-24 18:36:28 +0100

    chore: remove useless comments

--- src/components/Favicon.jsx
+++ src/components/Favicon.jsx
@@ -6,26 +6,22 @@ const Favicon = ({ countryCode }) => {
     let originalHref = null;
     let isLinkCreated = false;
 
-    // Créer ou récupérer la balise link existante
     if (!link) {
       link = document.createElement("link");
       link.rel = "icon";
       isLinkCreated = true;
     } else {
-      originalHref = link.href; // Sauvegarder l'URL originale
+      originalHref = link.href;
     }
 
-    // Mettre à jour l'URL de la favicon
     link.href = countryCode 
       ? `https://flagcdn.com/${countryCode}.svg`
       : "/favicon.png";
 
-    // Ajouter la balise si elle a été créée
     if (isLinkCreated) {
       document.head.appendChild(link);
     }
 
-    // Restaurer l'état original lors du nettoyage
     return () => {
       if (isLinkCreated) {
         link.remove();
